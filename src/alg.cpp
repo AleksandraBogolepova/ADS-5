9ba61577276c18871a07af2b4b0f88a0cf162454
AleksandraBogolepova <99176112+AleksandraBogolepova@users.noreply.github.com>
2022-04-04 19:37:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,90 @@
 #include <map>
 #include "tstack.h"
 
+int prior(char o){
+  switch (o) {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': case '-': return 2;
+    case '*': case '/': return 3;
+    case ' ': return 5;
+    default: return 4;
+  }
+}
+
+int calcul(char op,int a,int b){
+  switch (op){
+    case '+': return (b + a);
+    case '-': return (b - a);
+    case '*': return (b * a);
+    case '/':
+      if (a != 0)
+        return b / a;
+        default: return 0;
+  }
+}
+
 std::string infx2pstfx(std::string inf) {
   // добавьте код
-  return std::string("");
+
+    TStack <char, 100> stack;
+    std::string result = "";
+    for (int i = 0; i < inf.length(); i++) {
+        char sym = inf[i];
+        int symPrior = prior(sym);
+        if (symPrior == 4) {
+            result += sym;
+            result += ' ';
+        } else {
+            bool empty = stack.isEmpty();
+            int priorTop = prior(stack.get());
+            if (symPrior == 0 || symPrior > priorTop || empty) {
+                stack.push(sym);
+            } else if (symPrior == 1) {
+                while (prior(stack.get()) != 0) {
+                    result += stack.get();
+                    result += ' ';
+                    stack.pop();
+                }
+                 stack.pop();
+            } else if (symPrior <= prior(stack.get())) {
+                if (symPrior <= prior(stack.get())) {
+                    result += stack.get();
+                    result += ' ';
+                    stack.pop();
+                }
+                stack.push(sym);
+            }
+        }
+    }
+    if (!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
+            result += stack.get();
+            result += ' ';
+            stack.pop();
+        }
+        result.pop_back();
+    }
+    return result;
 }
 
 int eval(std::string pref) {
   // добавьте код
-  return 0;
+
+    TStack <int, 100> stack1;
+  int result = 0;
+  for (int i = 0; i < pref.length(); i++) {
+      char sym = pref[i];
+      if (prior(sym) == 4) {
+          stack1.push(sym - '0');
+      } else if (prior(sym) < 4) {
+          int temp1 = stack1.get();
+          stack1.pop();
+          int temp2 = stack1.get();
+          stack1.pop();
+          stack1.push(calcul(sym, temp1, temp2));
+      }
+  }
+  result = stack1.get();
+  return result;
 }
